e6135ea3380b5f4b45bb17709471e294fc38a1d7
Shahriyar001 <shahriyarmahbub55@gmail.com>
2022-10-26 20:35:17 +0600

    first commit

---
+++ src/firebase/Firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyC4DvaehySh7iiKfspfHIogOVsYHl7oDFs",
+    authDomain: "skills-booster-275eb.firebaseapp.com",
+    projectId: "skills-booster-275eb",
+    storageBucket: "skills-booster-275eb.appspot.com",
+    messagingSenderId: "940082811813",
+    appId: "1:940082811813:web:3cef483e51902f1ca7ce4c"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
